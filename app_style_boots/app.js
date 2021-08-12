32fd382d53f68db64d1cc0ebdebc33c51a62a858
Jean Sebastian Torres L <jtorres68@cuc.edu.co>
2021-08-12 17:09:03 -0500

    se termino el login, se termino toda la maquetacion en CSS del mismo y se agrego una linea route en el login

---
+++ app_style_boots/app.js
@@ -0,0 +1,11 @@
+
+
+function auth(){
+
+    if (document.form.password.value=='jtorres68' && document.form.login.value=='0006'){ 
+            document.form.submit(); 
+        } 
+        else{ 
+             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+        } 
+    } 
